6aec917a856725e4ac2dcfe50b88ebf1370949bd
yasmin-aline <yasminaline189@gmail.com>
2024-06-16 17:08:17 -0300

    adiciona a primeira versao

---
+++ mulher.js
@@ -0,0 +1,39 @@
+const express = require("express")
+
+const router = express.Router()
+
+
+
+const app = express()
+
+const porta = 3333
+
+
+
+function mostraMulher(request, response) {
+
+ response.json({
+
+    nome: 'Yasmin',
+
+   imagem: 'https//github.com/yasmin-aline.png',
+
+   minibio: 'Dev Backend',
+
+ })
+
+}
+
+
+function mostraPorta() {
+
+    console.log("Servidor criado e rodando na porta ", porta)
+
+}
+
+
+
+app.use(router.get('/mulher', mostraMulher))
+
+app.listen(porta, mostraPorta)
+
